a0c3830c819c420066faf8199add745c60f9ad47
LAPTOP-NTDNIKJF\think <doctor_2012_jiang@163.com>
2017-06-12 00:00:50 +0800

    用户

---
+++ src/main/java/com/lay/shop/modules/sys/web/UserController.java
@@ -0,0 +1,30 @@
+package com.lay.shop.modules.sys.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lay.shop.common.web.BaseController;
+import com.lay.shop.modules.sys.entity.User;
+
+/**
+ * 
+ * @author Lay
+ * @date 2017年6月9日 上午9:58:37
+ * @since
+ */
+@Controller
+@RequestMapping(value = "/sys/user")
+public class UserController extends BaseController {
+
+    @RequestMapping(value = "/login")
+    public String login() {
+
+        return null;        
+    }
+    
+    @RequestMapping(value = {"index"})
+    public String index(User user, Model model) {
+        return "modules/sys/userIndex";
+    }
+}
